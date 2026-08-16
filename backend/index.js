82056e3ef9ae6cbc862cc483629eda3590e41787
alokishere <alokvishwakarmabdh@gmail.com>
2026-08-17 01:26:43 +0530

    refactor: update CORS configuration and adjust axios baseURL for deployment

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,13 @@ connectDB()
         process.exit(1);
     });
 
-    
+
 const urlRouter = require("./routes/url.routes");
 const userRouter = require("./routes/user.routes");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [ "http://localhost:3000", "http://localhost:5173", "https://alokurl.vercel.app/" ],
   credentials: true,
 }));
 app.use(cookieParser());
