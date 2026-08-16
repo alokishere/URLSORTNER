5bcd9fb35cd9afe48ae62560ef503597588d4ee9
alokishere <alokvishwakarmabdh@gmail.com>
2026-08-17 01:19:48 +0530

    refactor: enhance MongoDB connection error handling and clean up commented code

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,14 +1,30 @@
+// const mongoose = require("mongoose");
+
+// async function connectDB() {
+//     await mongoose.connect(process.env.MONGO_URI)
+//     .then(() => {
+//         console.log("Connected to MongoDB");
+//     })
+//     .catch((err) => {
+//         console.error("Error connecting to MongoDB:", err);
+//     });
+
+// }
+
+// module.exports = connectDB;
+
+
+
 const mongoose = require("mongoose");
 
 async function connectDB() {
-    await mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.error("Error connecting to MongoDB:", err);
-    });
-
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
 }
 
 module.exports = connectDB;
